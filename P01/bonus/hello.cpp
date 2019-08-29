d00dd2d7d20240b27e7be0fccc977099a2d68459
Samarjit Singh Bons <samarjitsingh.bons@mavs.uta.edu>
2019-08-29 00:19:55 -0500

    bonus

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+cout << "What's your name?!" << endl;
+string name;
+getline(cin,name);
+cout << "Hello! "<< name << endl;
+
+
+}
