0dcb40a15c26172f004fb53d5f708a55dd599b9f
Samarjit Singh Bons <samarjitsingh.bons@mavs.uta.edu>
2019-10-11 14:02:22 -0500

    done

--- P07/full_credit/mainwin.cpp
+++ P07/full_credit/mainwin.cpp
@@ -1,11 +1,13 @@
 #include "mainwin.h"
+#include <cmath>
 
 Mainwin::Mainwin() 
 : scale{Gtk::manage(new Gtk::Scale)},
   frame{Gtk::manage(new Gtk::Frame{"Select an operation: "})},
-  radiobutton1{Gtk::manage(new Gtk::RadioButton{"Radio Button 1"})},
-  radiobutton2{Gtk::manage(new Gtk::RadioButton{"Radio Button 2"})},
-  radiobutton3{Gtk::manage(new Gtk::RadioButton{"Radio Button 3"})},
+  radiobutton1{Gtk::manage(new Gtk::RadioButton{"Square Root"})},
+  radiobutton2{Gtk::manage(new Gtk::RadioButton{"Natural Logarithm"})},
+  radiobutton3{Gtk::manage(new Gtk::RadioButton{"Trignometric Cosine"})},
+  radiobutton4{Gtk::manage(new Gtk::RadioButton{"extra"})},
   progressbar{Gtk::manage(new Gtk::ProgressBar)}
 
  {
@@ -27,15 +29,18 @@ Mainwin::Mainwin()
     vbox_frame->pack_start(*radiobutton1); // Note: Added to the *frame* above
     vbox_frame->pack_start(*radiobutton2);
     vbox_frame->pack_start(*radiobutton3);
+    //vbox_frame->pack_start(*radiobutton4);
     radiobutton1->join_group(*radiobutton2);
-    radiobutton2->join_group(*radiobutton3); // Put radios in the same group (unlimited permitted)
-    radiobutton1->signal_clicked().connect([this] {this->on_radiobutton_click(1);});
-    radiobutton2->signal_clicked().connect([this] {this->on_radiobutton_click(2);});
-    radiobutton3->signal_clicked().connect([this] {this->on_radiobutton_click(3);});
+    radiobutton3->join_group(*radiobutton2);
+    radiobutton4->join_group(*radiobutton2); // Put radios in the same group (unlimited permitted)
+    radiobutton1->signal_clicked().connect([this] {this->on_radiobutton_click(this->on_scale_value_changed(),1);});
+    radiobutton2->signal_clicked().connect([this] {this->on_radiobutton_click(this->on_scale_value_changed(),2);});
+    radiobutton3->signal_clicked().connect([this] {this->on_radiobutton_click(this->on_scale_value_changed(),3);});
+    radiobutton4->set_active();
 
     vbox->pack_start(*progressbar);
     progressbar->set_show_text(true);
-    progressbar->set_text("Adjust ProgressBar (below) with Scale (above)");
+    progressbar->set_text("Here's the result:");
     progressbar->set_fraction(0.0);
 
     vbox->show_all();
@@ -46,9 +51,42 @@ Mainwin::Mainwin()
 
 Mainwin::~Mainwin() { }
 
-void Mainwin::on_scale_value_changed() {
+int Mainwin::on_scale_value_changed() {
     int sp = scale->get_value();
     //label->set_text(std::to_string(sp*sp));
-    progressbar->set_fraction(sqrt(scale->get_value())/10.0);
+    //progressbar->set_fraction(sqrt(scale->get_value())/10.0);
+    return sp;
+}
+
+void Mainwin::on_radiobutton_click(int sp,int button) {
+    //Gtk::MessageDialog{*this, "Radio Button " + std::to_string(button)}.run();
+    double result;
+    if(button == 1){
+        result = sqrt(sp);
+        progressbar->set_fraction(result/100);
+        progressbar->set_text(std::to_string(result));
+        progressbar->set_show_text(true);
+
+    }
+    else if(button == 2){
+        result = log(sp);
+        progressbar->set_fraction(result/100);
+        progressbar->set_text(std::to_string(result));
+        progressbar->set_show_text(true);
+
+    }
+    else if(button == 3){
+        result = (cos(sp));
+        progressbar->set_fraction(result);
+        progressbar->set_text(std::to_string(result));
+        progressbar->set_show_text(true);
+
+    }
+}
+
+
+
+
+
 
 
--- P07/full_credit/mainwin.h
+++ P07/full_credit/mainwin.h
@@ -6,14 +6,15 @@ class Mainwin : public Gtk::Window {
     virtual ~Mainwin();
   protected:
     //void on_button_click();
-    void on_scale_value_changed();
-    void on_radiobutton_click(int button);
+    int on_scale_value_changed();
+    void on_radiobutton_click(int sp,int button);
   private:
     //Gtk::Button* button;
     Gtk::Scale* scale;
     Gtk::Frame* frame; // Contains the 3 radio buttons
     Gtk::RadioButton* radiobutton1;
     Gtk::RadioButton* radiobutton2;
-    Gtk::RadioButton* radiobutton3; 
+    Gtk::RadioButton* radiobutton3;
+    Gtk::RadioButton* radiobutton4; 
     Gtk::ProgressBar* progressbar;
 };
